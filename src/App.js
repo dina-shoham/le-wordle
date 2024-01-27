c9231e85c0dd6204cdc067277f5b8d8879715d08
dina-shoham <dina.shoham@gmail.com>
2024-01-27 16:13:39 +0100

    fixing some issues with fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Guess from './components/Guess';
 import GuessInput from './components/GuessInput';
 import './App.css';
 
 function App () {
   
-  const [word, setWord] = useState("");
+  const [solution, setSolution] = useState("");
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState(["hello", "world"]);
   
@@ -13,13 +13,29 @@ function App () {
   //   <li>{guess} x</li>
   // );
 
-  const generateWord = () => {
-    fetch('https://trouve-mot.fr/api/size/6')
-      .then(response => response.json())
-      .then(data => setWord(data[0].name))
-      .catch((error) => console.error(error));
-    console.log(word);
-  }
+  // call generate solution, empty array to ensure it only happens once
+  useEffect(() => {
+    async function generateSolution() {
+      try {
+        let response = await fetch('https://trouve-mot.fr/api/size/6');
+        response = await response.json();
+        console.log("response:");
+        console.log(response);
+        setSolution(response[0].name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    generateSolution();
+  }, []);
+
+  // const generateSolution = () => {
+  //   fetch('https://trouve-mot.fr/api/size/6')
+  //     .then(response => response.json())
+  //     .then(data => setSolution(data[0].name))
+  //     .catch((error) => console.error(error));
+  //   console.log(solution);
+  // }
 
   // function checkIfRealWord(word) {
   //   // check word's validity
@@ -38,12 +54,12 @@ function App () {
   return (
     <div>
       <h1>bienvenue au wordle</h1>
-      <div>
-        <button onClick={generateWord}>fetch word</button>
-      </div>
       <GuessInput/>
       <div>
       <Guess word='tester'/>
+      </div>
+      <div>
+        <p>solution is: {solution}</p>
       </div>
       {/* <p>bienvenue a le wordle</p>
       <br/>

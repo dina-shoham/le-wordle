b2c944c464710d8b669f913c031da884cb71b1ed
dina-shoham <dina.shoham@gmail.com>
2024-02-19 17:18:08 +0100

    enter and delete functionality

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import "toastify-js/src/toastify.css"
 import './styles.css';
 
 function App () {
+  /*=================================================================
+    VARIABLES
+  =================================================================*/
   // var currentGuess = "";
   var guessIndex = -1;
   var emptyStatusArr = [-1, -1, -1, -1, -1, -1];
-  
+  var isFrenchWord = true;
+  // statuses: -1 = not yet verified, 0 = incorrect, 1 = yellow, 2 = green
+
   const [solution, setSolution] = useState("");
   const [guessNumber, setGuessNumber] = useState(-1);
-  // const [guessIndex, setGuessIndex] = useState(-1);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guessingDisabled, setGuessingDisabled] = useState(false);
-  // statuses: -1 = not yet verified, 0 = incorrect, 1 = yellow, 2 = green
   const [guesses, setGuesses] = useState([{word: "      ", status: emptyStatusArr},
                                            {word: "      ", status: emptyStatusArr},
                                            {word: "      ", status: emptyStatusArr},
@@ -24,7 +27,10 @@ function App () {
                                            {word: "      ", status: emptyStatusArr},
                                            {word: "      ", status: emptyStatusArr}                                           
                                          ]);
-
+  
+  /*=================================================================
+    SETUP
+  =================================================================*/
   // call generate solution, empty array to ensure it only happens once
   // https://stackoverflow.com/questions/53332321/react-hook-warnings-for-async-function-in-useeffect-useeffect-function-must-ret
   useEffect(() => {
@@ -33,6 +39,7 @@ function App () {
         let response = await fetch('https://trouve-mot.fr/api/size/6');
         response = await response.json();
         setSolution(response[0].name);
+        console.log((response[0].name));
       } catch (error) {
         console.error(error);
       }
@@ -41,17 +48,32 @@ function App () {
     setGuessNumber(1);
   }, []);
 
+  /*=================================================================
+    TYPING GUESS
+  =================================================================*/
   // keyboard listener for typing in guesses
   useEffect(() => {
     const keyDownHandler = event => {
       if (guessIndex < 5) {
         // ensure key is in alphabet (qwerty: 65-90)
         if (event.keyCode >= 65 && event.keyCode <= 90) {
-          console.log("letter typed");
           updateCurrentGuess(event.key);
         }
         // todo: handle azerty keyboard accent keys
         // azerty: 65-90 and 50, 55, 57, 48, 192, 229
+
+        // backspace
+        if (event.keyCode == 8) {
+          handleBackspace();
+        }
+      }
+
+      if (guessIndex == 5) {
+        // enter
+        if (event.keyCode == 13) {
+          console.log("enter");
+          verifyGuess();
+        }
       }
     }
     document.addEventListener('keydown', keyDownHandler);
@@ -66,9 +88,14 @@ function App () {
     setCurrentGuess(currentGuess => currentGuess.substring(0, guessIndex) + char + currentGuess.substring(guessIndex + 1));
   };
   
+  // handle backspace event
+  function handleBackspace() {
+    setCurrentGuess(currentGuess => currentGuess.substring(0, currentGuess.length - 1));
+    decrementGuessIndex();
+  };
+
   // render current guess on grid
   useEffect(() => {
-    console.log("here");
     var guessPadded = currentGuess;
     // pad guess with spaces
     while(guessPadded.length < 6) {
@@ -81,6 +108,20 @@ function App () {
     setGuesses(() => guessesTmp);
   }, [currentGuess])
   
+  function incrementGuessIndex() {
+    guessIndex = guessIndex + 1;
+    // setGuessIndex(guessIndex => guessIndex + 1);
+    console.log("incr");
+  };
+
+  function decrementGuessIndex() {
+    guessIndex = guessIndex - 1;
+    console.log("decr");
+  };
+
+  /*=================================================================
+    SUBMITTING GUESS
+  =================================================================*/
   // update guesses list
   function updateGuessList(guess) {
     setGuesses([
@@ -89,17 +130,60 @@ function App () {
   };
   
   // self explanatory i hope
-  function incrementGuessIndex() {
-    guessIndex = guessIndex + 1;
-    // setGuessIndex(guessIndex => guessIndex + 1);
-    console.log("incr");
-  };
-  
-  // self explanatory i hope
   function incrementGuessNumber() {
     setGuessNumber(guessNumber => guessNumber + 1);
   };
 
+  // verify guess
+  async function verifyGuess() {
+    var guess = currentGuess;
+    console.log("you guessed " + guess);
+
+    await checkIfFrenchWord();      
+    console.log(`is it a real word??? ${isFrenchWord}`);
+    
+    if (isFrenchWord && guess.length === 6) {
+      let verifArray = emptyStatusArr;
+
+      // verify word against solution
+      for (let i = 0; i < guess.length; i++) {
+        // green
+        if (solution[i] === guess[i]) {
+          verifArray[i] = 2; // green
+          continue;
+        // yellow
+        } else if (solution.includes(guess[i])) {
+          verifArray[i] = 1; // yellow
+        } else {
+            verifArray[i] = 0; // grey
+        }
+
+        // todo: duplicate letter handling
+        // todo: accent handling
+      }
+    }
+  };
+
+  async function checkIfFrenchWord (word) {
+    console.log(`checking if ${word} a real word!`);
+    const url = `https://fr.wiktionary.org/w/api.php?action=query&format=json&titles=${word}`;
+
+    try {
+        let response = await fetch(url);
+        response = await response.json();
+        // if page does not exist
+        if('-1' in response.query.pages) {
+            console.log("pg does not exist");
+            isFrenchWord = false;
+        }
+    } catch (error) {
+        console.error(error);
+      }        
+  };
+
+  /*=================================================================
+    GAME FUNCTIONS (winning, losing)
+  =================================================================*/
   // lose after 7 guesses
   useEffect(() => {
     if (guessNumber > 7) {
@@ -153,6 +237,10 @@ function App () {
     }
   }, [guesses]);
 
+
+  /*=================================================================
+    RENDER
+  =================================================================*/
   return (
     <div>
       <h1>le wordle</h1>
